9f61fc707a9f5be52f154daf011bf7d26dae9d03
neumann-wj <julian.neumann@charite.de>
2020-02-05 15:59:19 +0100

    added filetree

--- icn_tb/icn_tb.py
+++ icn_tb/icn_tb.py
@@ -8,6 +8,17 @@ import psutil
 from matplotlib import pyplot as plt
 
 
+
+def list_files(startpath):
+    for root, dirs, files in os.walk(startpath):
+        level = root.replace(startpath, '').count(os.sep)
+        indent = ' ' * 4 * (level)
+        print('{}{}/'.format(indent, os.path.basename(root)))
+        subindent = ' ' * 4 * (level + 1)
+        for f in files:
+            print('{}{}'.format(subindent, f))
+            
+
 def ci(srchlist, strlist):
     if isinstance(srchlist, type('a')):
         srchlist = [srchlist]
