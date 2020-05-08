f8d19d86e8c57b09b9e7fc5fe99e9b5d24d1e3c4
vpeterson <victoriapeterson09@gmail.com>
2020-05-08 11:51:32 -0300

    updated rereference given dataframe

--- icn_m1/rereference.py
+++ icn_m1/rereference.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 
 # def rereference(bv_raw, df_channel):
 #     # given used and predictor column, rereference those that have used = 1 and predictor = 0 acc to CAR and BR 
@@ -17,7 +18,7 @@ import numpy as np
 #             bv_raw_r[ch,:] -= avg_
 #         elif:
 
-def rereference(raw_data, channels_name, ref_channels='average'):
+def rereference(raw_data, run_string):
     """
     
 
@@ -25,11 +26,8 @@ def rereference(raw_data, channels_name, ref_channels='average'):
     ----------
     raw_dat : array, shape(n_channels, n_samples)
         raw_data to be epoched.
-    channels_name: list of strings
-        name of the channels in raw_data
-    ref_channels : string, list of strings
-        channel(s) to which rerefenrence raw_data. The default is 'average' 
-        (common average reference)
+    run_string (string): run string without specific ending in form sub-000_ses-right_task-force_run-0
+
     
     Returns
     -------
@@ -37,18 +35,28 @@ def rereference(raw_data, channels_name, ref_channels='average'):
         rereferenced raw_data.
 
     """
+    
+    df_channel = pd.read_csv(run_string + "_channels_M1.tsv", sep="\t")
+    used_ch = np.where(df_channel['used'] == 1)[0]
+    non_target_ch = np.where(df_channel['target'] == 0)[0]
+
+    index_channels = np.intersect1d(used_ch, non_target_ch)
+
     new_raw_data=raw_data.copy()
    
-
-    index_channels=np.arange(0,len(channels_name))
-    for i in range(len(channels_name)):
+    channels_name=df_channel['name'].tolist()
+    for i in index_channels:
+        #print(df_channel['name'][i])
+        
         elec_channel=index_channels==i
         ch=raw_data[elec_channel,:]
-        if ref_channels=='average':
+        if df_channel['rereference'][i] == 'average':
             av=np.mean(raw_data[index_channels!=i,:], axis=0)
             new_raw_data[i]=ch-av
         else:
             index=[]
+            ref_channels=df_channel['rereference'][i].split('+')
+
             for j in range(len(ref_channels)):
                 if ref_channels[j] not in channels_name:
                     raise ValueError('One or maybe more of the ref_channels are not part of the recording channels.')
