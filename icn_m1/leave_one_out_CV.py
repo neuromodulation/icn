da5978b59b97271e6c72c99b89f43d5c9666aeef
vpeterson <victoriapeterson09@gmail.com>
2020-05-29 16:35:52 -0300

    updated to new derivates

--- icn_m1/leave_one_out_CV.py
+++ icn_m1/leave_one_out_CV.py
@@ -2,7 +2,6 @@ import os
 import numpy as np
 import settings
 import pickle
-import settings
 
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import cross_val_score
@@ -10,19 +9,54 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import r2_score, roc_auc_score
 from sklearn.ensemble import RandomForestRegressor
 import multiprocessing
+import json
+import sys
+import settings
 
+
+VICTORIA = True
+
+
+settings = {}
+
+if VICTORIA is True:
+    # insert at 1, 0 is the script path (or '' in REPL)
+    sys.path.insert(1, '/home/victoria/icn/icn_m1')
+    settings['BIDS_path'] = "/mnt/Datos/BML_CNCRS/Data_BIDS/"
+    settings['out_path'] = "/mnt/Datos/BML_CNCRS/Data_processed"
+else:
+    settings['BIDS_path'] = "C:\\Users\\ICN_admin\\Dropbox (Brain Modulation Lab)\\Shared Lab Folders\\CRCNS\\MOVEMENT DATA\\"
+    settings['out_path'] = "C:\\Users\\ICN_admin\\Documents\\Decoding_Toolbox\\gen_p_files\\"
+
+settings['resamplingrate']=10
+settings['max_dist_cortex']=20
+settings['max_dist_subcortex']=5
+settings['normalization_time']=10
+settings['num_patients']=17
+
+settings['frequencyranges']=[[4, 8], [8, 12], [13, 20], [20, 35], [13, 35], [60, 80], [90, 200], [60, 200]]
+settings['seglengths']=[1, 2, 2, 3, 3, 3, 10, 10, 10]
+
+
+settings['BIDS_path']=settings['BIDS_path'].replace("\\", "/")
+settings['out_path']=settings['out_path'].replace("\\", "/")
+
+with open('settings/settings.json', 'w') as fp:
+    json.dump(settings, fp)
+
+#%%
 def get_int_runs(patient_idx):
     """
 
     :param patient_idx:
     :return: list with all run files for the given patient
     """
-    os.listdir(settings.out_path_folder_downsampled)
+    os.listdir(settings['out_path'])
     if patient_idx < 10:
         subject_id = str('00') + str(patient_idx)
     else:
         subject_id = str('0') + str(patient_idx)
-    list_subject = [i for i in os.listdir(settings.out_path_folder_downsampled) if i.startswith('sub-'+subject_id) and i.endswith('.p')]
+    list_subject = [i for i in os.listdir(settings['out_path']) if i.startswith('sub_'+subject_id) and i.endswith('.p')]
     return list_subject
 
 
@@ -36,10 +70,10 @@ def get_act_int_list(patient_idx):
     runs_ = get_int_runs(patient_idx)
     act_ = np.zeros([len(runs_), 94])
     for idx in range(len(runs_)):
-        file = open(os.path.join(settings.out_path_folder, runs_[idx]), 'rb')
+        file = open(os.path.join(settings['out_path'], runs_[idx]), 'rb')
         out = pickle.load(file)
 
-        act_[idx, :] = out['act_grid_points']
+        act_[idx, :] = out['arr_act_grid_points']
 
     return act_
 
@@ -51,7 +85,7 @@ def save_all_act_grid_points():
     """
     l_act = []
     
-    for patient_idx in range(settings.num_patients):
+    for patient_idx in range(settings['num_patients']):
         l_act.append(get_act_int_list(patient_idx))
     np.save('act_.npy', np.array(l_act))
     return np.array(l_act)
@@ -65,15 +99,11 @@ def check_leave_out_grid_points(act_, load=True):
         return np.load('grid_points_none.npy', allow_pickle=True)
     
     grid_point_occurance = np.zeros(94)
-    for patient_idx in range(settings.num_patients):
+    for patient_idx in range(settings['num_patients']):
         grid_point_occurance[np.where(np.sum(act_[patient_idx], axis=0))[0]] += 1
     grid_points_none = np.where((grid_point_occurance == 0) | (grid_point_occurance == 1))[0]
     np.save('grid_points_none.npy', grid_points_none)
     return grid_points_none
-    
-
-act_ = save_all_act_grid_points()
-grid_points_none = check_leave_out_grid_points(act_, False)
 
 def append_time_dim(arr, y_, time_stamps):
     """
@@ -88,14 +118,28 @@ def append_time_dim(arr, y_, time_stamps):
 def get_train_test_dat(patient_test, grid_point, act_, Train=True):
     """
     For a given grid_point, and a given provided test patient, acquire all combined dat and label information
-    :param patient_test:
-    :param grid_point:
-    :param act_:
-    :param Train: determine if data is returned only from patient_test, or from all other
-    :return: concatenated dat, label
+    
+    Parameters
+    ----------
+    patient_test : TYPE
+        DESCRIPTION.
+    grid_point : TYPE
+        DESCRIPTION.
+    act_ : TYPE
+        DESCRIPTION.
+    Train : TYPE, optional
+        etermine if data is returned only from patient_test, or from all other. The default is True.
+
+    Returns
+    -------
+    dat : array shape(n_data points,n_frequency bands)
+        DESCRIPTION.
+    label : TYPE
+        DESCRIPTION.
+
     """
     start = 0
-    for patient_idx in range(settings.num_patients):
+    for patient_idx in range(settings['num_patients']):
         if Train is True and patient_idx == patient_test:
             continue
         if Train is False and patient_idx != patient_test:
@@ -107,37 +151,38 @@ def get_train_test_dat(patient_test, grid_point, act_, Train=True):
                 # does this run has the grid point?
                 if act_[patient_idx][run_idx, grid_point] != 0:
                     # load file
-                    file = open(os.path.join(settings.out_path_folder_downsampled, run), 'rb')
+                    file = open(os.path.join(settings['out_path'], run), 'rb')
                     out = pickle.load(file)
 
                     # fill dat
                     if start == 0:
-                        dat = out['int_data'][grid_point]
+                        dat = out['pf_data_median'][:,grid_point,:]
                         if grid_point < 39 or (grid_point > 78 and grid_point < 86):  # contralateral
-                            label = out['label_mov'][0, :]
+                            label = np.squeeze(out['label'][out['label_con_true']==True])
                         else:
-                            label = out['label_mov'][1, :]
+                            label = np.squeeze(out['label'][out['label_con_true']==False])
                         start = 1
                     else:
-                        dat = np.concatenate((dat, out['int_data'][grid_point]), axis=1)
+                        dat = np.concatenate((dat, out['pf_data_median'][:,grid_point,:]), axis=0)
 
                         if grid_point < 39 or (grid_point > 78 and grid_point < 86):  # contralateral
-                            label = np.concatenate((label, out['label_mov'][0, :]), axis=0)
+                            label_new=np.squeeze(out['label'][out['label_con_true']==True])
+                            label = np.concatenate((label, label_new), axis=0)
                         else:
-                            label = np.concatenate((label, out['label_mov'][1, :]), axis=0)
+                            label_new=np.squeeze(out['label'][out['label_con_true']==False])
+                            label = np.concatenate((label, label_new), axis=0)
     return dat, label
 
 
-def train_grid_point(time_stamps, act_, patient_test, grid_point, model_fun):
-    
-    print(grid_point)
+def train_grid_point(time_stamps, act_, patient_test, grid_point, model, Verbose=False):
+    if Verbose:
+        print(grid_point)
     dat, label = get_train_test_dat(patient_test, grid_point, act_, Train=True)
-    dat,label = append_time_dim(dat.T, label,time_stamps)
+    dat,label = append_time_dim(dat, label,time_stamps)
 
     dat_test, label_test = get_train_test_dat(patient_test, grid_point, act_, Train=False)
-    dat_test,label_test = append_time_dim(dat_test.T, label_test, time_stamps)
+    dat_test,label_test = append_time_dim(dat_test, label_test, time_stamps)
 
-    model = model_fun(n_estimators=32, max_depth=4)
     model.fit(dat, label)
 
     y_test_pred = model.predict(dat_test)
@@ -154,7 +199,7 @@ def train_grid_point(time_stamps, act_, patient_test, grid_point, model_fun):
 
     return predict_
 
-def run_CV(patient_test, model_fun = RandomForestRegressor, time_stamps=5):
+def run_CV(patient_test, model=LinearRegression(), time_stamps=5):
     """
     given model is trained grid point wise for the provided patient
     saves output estimations and labels in a struct with r2 correlation coefficient
@@ -173,21 +218,22 @@ def run_CV(patient_test, model_fun = RandomForestRegressor, time_stamps=5):
     for grid_point in np.nonzero(arr_active_grid_points)[0]:
         if grid_point in grid_points_none:
             continue
-        patient_CV_out[grid_point] = train_grid_point(time_stamps, act_, patient_test, grid_point, model_fun)
+        patient_CV_out[grid_point] = train_grid_point(time_stamps, act_, patient_test, grid_point, model)
         
     if patient_test < 10:
         subject_id = str('00') + str(patient_test)
     else:
         subject_id = str('0') + str(patient_test)
 
-    out_path_file = os.path.join(settings.out_path_folder_downsampled, subject_id+'prediction.npy')
+    out_path_file = os.path.join(settings['out_path'], subject_id+'prediction.npy')
     np.save(out_path_file, patient_CV_out)
+#%%
+patients=17
+    
+act_ = save_all_act_grid_points()
+grid_points_none = check_leave_out_grid_points(act_, False)
 
 if __name__== "__main__":
 
-    #run_CV(patient_test=16, model_fun = RandomForestRegressor, time_stamps=5)
-
-    #save_all_act_grid_points()
-
     pool = multiprocessing.Pool()
-    pool.map(run_CV, np.arange(0, 16, 1))
+    pool.map(run_CV, np.arange(patients))
