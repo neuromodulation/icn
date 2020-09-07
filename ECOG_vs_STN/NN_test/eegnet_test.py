26131708137971c004656df464d947ef077c2626
timonmerk <timon.merk95@gmail.com>
2020-09-07 11:46:45 +0200

    add deep convnet

--- ECOG_vs_STN/NN_test/eegnet_test.py
+++ ECOG_vs_STN/NN_test/eegnet_test.py
@@ -37,7 +37,6 @@ PATH = "C:\\Users\\ICN_admin\\Dropbox (Brain Modulation Lab)\\Shared Lab Folders
 vhdr_files = IO.get_all_vhdr_files(PATH)
 
 
-
 # custom definition of cost fct.
 def r2_keras(y_true, y_pred):
     SS_res =  K.sum(K.square(y_true - y_pred))
@@ -152,6 +151,72 @@ def EEGNet(Chans = 6, Samples = 1000,
     linear      = Activation('linear', name = 'linear')(dense)
 
     return Model(inputs=input1, outputs=linear)
+
+def DeepConvNet(Chans = 64, Samples = 256,
+                dropoutRate = 0.5):
+    """ Keras implementation of the Deep Convolutional Network as described in
+    Schirrmeister et. al. (2017), Human Brain Mapping.
+
+    This implementation assumes the input is a 2-second EEG signal sampled at
+    128Hz, as opposed to signals sampled at 250Hz as described in the original
+    paper. We also perform temporal convolutions of length (1, 5) as opposed
+    to (1, 10) due to this sampling rate difference.
+
+    Note that we use the max_norm constraint on all convolutional layers, as
+    well as the classification layer. We also change the defaults for the
+    BatchNormalization layer. We used this based on a personal communication
+    with the original authors.
+
+                      ours        original paper
+    pool_size        1, 2        1, 3
+    strides          1, 2        1, 3
+    conv filters     1, 5        1, 10
+
+    Note that this implementation has not been verified by the original
+    authors.
+
+    """
+
+    # start the model
+    input_main   = Input((1, Chans, Samples))
+    block1       = Conv2D(25, (1, 5),
+                                 input_shape=(1, Chans, Samples),
+                                 kernel_constraint = max_norm(2., axis=(0,1,2)))(input_main)
+    block1       = Conv2D(25, (Chans, 1),
+                                 kernel_constraint = max_norm(2., axis=(0,1,2)))(block1)
+    block1       = BatchNormalization(axis=1, epsilon=1e-05, momentum=0.1)(block1)
+    block1       = Activation('elu')(block1)
+    block1       = MaxPooling2D(pool_size=(1, 2), strides=(1, 2))(block1)
+    block1       = Dropout(dropoutRate)(block1)
+
+    block2       = Conv2D(50, (1, 5),
+                                 kernel_constraint = max_norm(2., axis=(0,1,2)))(block1)
+    block2       = BatchNormalization(axis=1, epsilon=1e-05, momentum=0.1)(block2)
+    block2       = Activation('elu')(block2)
+    block2       = MaxPooling2D(pool_size=(1, 2), strides=(1, 2))(block2)
+    block2       = Dropout(dropoutRate)(block2)
+
+    block3       = Conv2D(100, (1, 5),
+                                 kernel_constraint = max_norm(2., axis=(0,1,2)))(block2)
+    block3       = BatchNormalization(axis=1, epsilon=1e-05, momentum=0.1)(block3)
+    block3       = Activation('elu')(block3)
+    block3       = MaxPooling2D(pool_size=(1, 2), strides=(1, 2))(block3)
+    block3       = Dropout(dropoutRate)(block3)
+
+    block4       = Conv2D(200, (1, 5),
+                                 kernel_constraint = max_norm(2., axis=(0,1,2)))(block3)
+    block4       = BatchNormalization(axis=1, epsilon=1e-05, momentum=0.1)(block4)
+    block4       = Activation('elu')(block4)
+    block4       = MaxPooling2D(pool_size=(1, 2), strides=(1, 2))(block4)
+    block4       = Dropout(dropoutRate)(block4)
+
+    flatten      = Flatten()(block4)
+
+    dense        = Dense(1, kernel_constraint = max_norm(0.5))(flatten)
+    #softmax      = Activation('softmax')(dense)
+    linear      = Activation('linear', name = 'linear')(dense)
+
+    return Model(inputs=input_main, outputs=linear)
 
 def get_data_raw_combined(sub, sess, loc ,f_):
 
