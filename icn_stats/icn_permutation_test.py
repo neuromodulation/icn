bbc2dc414d9461655a1fa00b403bbc4f0471bfeb
timonmerk <timon.merk95@gmail.com>
2020-10-23 12:45:02 +0200

    stat permutation fix rho

--- icn_stats/icn_permutation_test.py
+++ icn_stats/icn_permutation_test.py
@@ -10,48 +10,52 @@ import statsmodels.api as sm
 import statsmodels.formula.api as smf
 
 ### Write this as a function
-def permutationTestMedianSplit(x, y, plot_=True, x_unit=None, p=5000):
+def permutationTestSpearmansRho(x, y, plot_=True, x_unit=None, p=5000):
     """
-    Calculate permutation test
+    Calculate permutation test for multiple repetitions of Spearmans Rho
     https://towardsdatascience.com/how-to-assess-statistical-significance-in-your-data-with-permutation-tests-8bb925b2113d
 
-    x (np array) : distribution that will be compared statistically e.g. R^2
-    y (np array) : according ordinal values e.g. UPDRS
+    x (np array) : first distibution e.g. R^2
+    y (np array) : second distribution e.g. UPDRS
     plot_ (boolean) : if True: permutation histplot and ground truth will be potted
     x_unit (str) : histplot xlabel
     p (int): number of permutations
+
+    returns:
+    gT (float) : estimated ground truth, here spearman's rho
+    p (float) : p value of permutation test
     """
 
-    # first, order y, and take respective values of x
-    arr_argsort = x[np.argsort(y)]
-
     # compute ground truth difference
-    gT = np.abs(np.average(arr_argsort[:int(arr_argsort.shape[0]/2)]) -
-            np.average(arr_argsort[int(arr_argsort.shape[0]/2):]))
-
+    gT = stats.spearmanr(x, y)[0]
     #
-    pV = arr_argsort
-    pS = copy.copy(pV)
+    pV = np.array((x,y))
     #Initialize permutation:
     pD = []
     # Permutation loop:
+    args_order = np.arange(0,pV.shape[1],1)
+    args_order_2 = np.arange(0,pV.shape[1],1)
     for i in range(0,p):
       # Shuffle the data:
-        random.shuffle(pS)
+        random.shuffle(args_order)
+        random.shuffle(args_order_2)
         # Compute permuted absolute difference of your two sampled distributions and store it in pD:
-        pD.append(np.abs(np.average(pS[0:int(len(pS)/2)]) - np.average(pS[int(len(pS)/2):])))
+        pD.append(stats.spearmanr(pV[0,args_order], pV[1,args_order_2])[0])
 
     # calculate p value
-    p_val = len(np.where(pD>=gT)[0])/p
+    if gT < 0:
+        p_val = len(np.where(pD<=gT)[0])/p
+    else:
+        p_val = len(np.where(pD>=gT)[0])/p
 
     if plot_ is True:
         plt.hist(pD, bins=30,label="permutation results")
         plt.axvline(gT, color="orange", label="ground truth")
-        plt.title("p="+str(p_val))
-        plt.xlabel(r"$R^2$")
+        plt.title("ground truth "+x_unit+"="+str(gT)+" p="+str(p_val))
+        plt.xlabel(x_unit)
         plt.legend()
         plt.show()
-    return p_val
+    return gT, p_val
 
     ### Now write a second version where two distributions are compared
     ### Write this as a function
@@ -65,6 +69,11 @@ def permutationTest(x, y, plot_=True, x_unit=None, p=5000):
     plot_ (boolean) : if True: permutation histplot and ground truth will be potted
     x_unit (str) : histplot xlabel
     p (int): number of permutations
+
+    returns:
+    gT (float) : estimated ground truth, here abs difference of distribution means
+    p (float) : p value of permutation test
+
     """
 
 
@@ -84,13 +93,16 @@ def permutationTest(x, y, plot_=True, x_unit=None, p=5000):
         pD.append(np.abs(np.average(pS[0:int(len(pS)/2)]) - np.average(pS[int(len(pS)/2):])))
 
     # calculate p value
-    p_val = len(np.where(pD>=gT)[0])/p
+    if gT < 0:
+        p_val = len(np.where(pD<=gT)[0])/p
+    else:
+        p_val = len(np.where(pD>=gT)[0])/p
 
     if plot_ is True:
         plt.hist(pD, bins=30,label="permutation results")
         plt.axvline(gT, color="orange", label="ground truth")
-        plt.title("p="+str(p_val))
-        plt.xlabel(r"$R^2$")
+        plt.title("ground truth "+x_unit+"="+str(gT)+" p="+str(p_val))
+        plt.xlabel(x_unit)
         plt.legend()
         plt.show()
-    return p_val
+    return gT, p_val
