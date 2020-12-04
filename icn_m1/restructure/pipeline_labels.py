f4c1fecdbcf579f17e1a57b2fad4c7e87c21844b
timonmerk <timon.merk95@gmail.com>
2020-12-04 13:35:44 +0100

    exclude nb checkpoints new

--- icn_m1/restructure/pipeline_labels.py
+++ icn_m1/restructure/pipeline_labels.py
@@ -9,6 +9,10 @@ import pandas as pd
 import os
 import run_analysis
 
+# reads saved feature file by pipeline_features 
+# reads M1, settings, dat 
+
+
 ### READ M1.tsv 
 PATH_M1 = "C:\\Users\\ICN_admin\\Charité - Universitätsmedizin Berlin\\Interventional Cognitive Neuromodulation - Data\\Datasets\\BIDS Berlin\\sub-002\\ses-20200131\\ieeg\\sub-002_ses-20200131_task-selfpacedrotation202001310001_run-4_channels_M1.tsv"
 df_M1 = pd.read_csv(PATH_M1, sep="\t") 
@@ -37,44 +41,8 @@ fs = int(np.ceil(fs))
 ### READ line noise (could be done by mne_bids.read_raw_bids)
 line_noise = IO.read_line_noise(settings['BIDS_path'], subject) # line noise is a column in the participants.tsv
 
-### CALCULATE filter
-# well here I needed t add to the filter_len 1, need to recheck if that's every time neccessary!
-filter_fun = filter.calc_band_filters(settings['frequencyranges'], sample_rate=fs, filter_len=fs+1)
+# estimate baseline correction 
 
-### DEFINE generator
-def ieeg_raw_generator(ieeg_raw, df_M1, settings, fs):
-    """[summary]
+# READ feature file and write corrected baseline as additional key
 
-    Args:
-        ieeg_raw (np array): shape (channels, time)
-        fs (float): 
-        fs_new (float): new resampled frequency 
-        offset_start (int): size of highest segmenth length, needs to be skipped at the start to have same feature size
-
-    Yields:
-        np.array: new batch for run function of full segment length shape
-    """
-
-    cnt_fsnew = 0
-    offset_start = int(settings["seglengths"][0] * fs)
-    fs_new = settings["resamplingrate"]
-    used_idx = df_M1[(df_M1["used"] == 1) & (df_M1["target"] == 0)].index
-    
-    for cnt in range(ieeg_raw.shape[1]):
-        if cnt < offset_start:
-            cnt_fsnew +=1
-            continue
-        
-        cnt_fsnew +=1
-        if cnt_fsnew >= (fs/fs_new):
-            cnt_fsnew = 0
-            yield ieeg_raw[used_idx,cnt-offset_start:cnt]
-
-### INITIALIZE generator 
-gen_ = ieeg_raw_generator(ieeg_raw[:,:20000], df_M1, settings, fs) # clip for timing reasons
-
-### CALL run function 
-data_features = run_analysis.run(gen_, settings, df_M1, fs, line_noise, filter_fun, usemean_=True, normalize=True)
-
-# SAVE object
-# CALL plotting functions
+# call plot_ieeg --> save figure, maybe create new run folder 
