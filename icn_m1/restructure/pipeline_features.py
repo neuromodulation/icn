70042832327f911432e533a0acc46b5ec7e010e0
timonmerk <timon.merk95@gmail.com>
2021-01-04 20:46:40 +0100

    allow suffix and prefix param to be str

--- icn_m1/restructure/pipeline_features.py
+++ icn_m1/restructure/pipeline_features.py
@@ -16,21 +16,21 @@ import mne_bids
 # save as mat / p 
 
 ### READ M1.tsv 
-PATH_M1 = "C:\\Users\\ICN_admin\\Charité - Universitätsmedizin Berlin\\Interventional Cognitive Neuromodulation - Data\\Datasets\\BIDS Berlin\\sub-002\\ses-20200131\\ieeg\\sub-002_ses-20200131_task-selfpacedrotation202001310001_run-4_channels_M1.tsv"
-df_M1 = pd.read_csv(PATH_M1, sep="\t") 
+PATH_M1 = r"C:\Users\ICN_admin\Charité - Universitätsmedizin Berlin\Interventional Cognitive Neuromodulation - Data\Datasets\BIDS_Berlin\derivatives\sub-001\ses-20171014\ieeg\sub-001_ses-20171014_task-selfpacedforcewheelON_run-0_channels_M1.tsv"
+df_M1 = pd.read_csv(PATH_M1, sep="\t")
 
 ### READ settings
 with open('settings\\settings.json', 'rb') as f:
         settings = json.load(f)
-settings["BIDS_path"] = "C:\\Users\\ICN_admin\\Charité - Universitätsmedizin Berlin\\Interventional Cognitive Neuromodulation - Data\\Datasets\\BIDS Berlin"
+settings["BIDS_path"] = "C:\\Users\\ICN_admin\\Charité - Universitätsmedizin Berlin\\Interventional Cognitive Neuromodulation - Data\\Datasets\\BIDS_Berlin"
 
 ### SPECIFY iEEG file to read (INPUT to pipeline.py)
-ieeg_files = IO.get_all_ieeg_files(settings['BIDS_path']) # all files...
-run_file_to_read = "C:\\Users\\ICN_admin\\Charité - Universitätsmedizin Berlin\\Interventional Cognitive Neuromodulation - Data\\Datasets\\BIDS Berlin\\sub-002\\ses-20200131\\ieeg\\sub-002_ses-20200131_task-selfpacedrotation202001310001_run-4_ieeg.vhdr"
+ieeg_files = IO.get_all_files(settings['BIDS_path'], suffix='vhdr') # all files...
+run_file_to_read = "C:\\Users\\ICN_admin\\Charité - Universitätsmedizin Berlin\\Interventional Cognitive Neuromodulation - Data\\Datasets\\BIDS_Berlin\\sub-002\\ses-20200131\\ieeg\\sub-002_ses-20200131_task-selfpacedrotation202001310001_run-4_ieeg.vhdr"
 subject, sess, task, run = IO.get_subject_sess_task_run(os.path.basename(run_file_to_read))
 
 ### READ BIDS data
-ieeg_raw, ch_names = IO.read_BIDS_file(run_file_to_read)
+#ieeg_raw, ch_names = IO.read_BIDS_file(run_file_to_read)
 
 ### BETTER: 
 bids_read_path = mne_bids.BIDSPath(subject=subject, session=sess, \
@@ -39,15 +39,17 @@ raw_arr = mne_bids.read_raw_bids(bids_read_path)
 ieeg_raw = raw_arr.get_data()
 
 ### READ Coordinates
-df_coord = pd.read_csv(os.path.join(os.path.dirname(run_file_to_read), \
-                                    "sub-"+subject+"_electrodes.tsv"), sep="\t")
+# EDIT: coordinates are read though the mne_bids read_raw_bids function
+#df_coord = pd.read_csv(os.path.join(os.path.dirname(run_file_to_read), \
+#                                    "sub-"+subject+"_electrodes.tsv"), sep="\t")
 
 ### READ sampling frequency (could be done by mne_bids.read_raw_bids)
 fs=IO.read_run_sampling_frequency(run_file_to_read)[0] 
 fs = int(np.ceil(fs))
 
 ### READ line noise (could be done by mne_bids.read_raw_bids)
-line_noise = IO.read_line_noise(settings['BIDS_path'], subject) # line noise is a column in the participants.tsv
+line_noise = int(raw_arr.info["line_freq"])
+#line_noise = IO.read_line_noise(settings['BIDS_path'], subject) # line noise is a column in the participants.tsv
 
 ### CALCULATE filter
 # well here I needed t add to the filter_len 1, need to recheck if that's every time neccessary!
