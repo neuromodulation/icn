75fdb45408404464c86f78bc908795f275d241e9
Saeed Salehi <49148519+ssnio@users.noreply.github.com>
2021-03-12 21:40:15 +0100

    Update plotly_html.py

--- icn_plots/plotly_html.py
+++ icn_plots/plotly_html.py
@@ -1,9 +1,3 @@
-import numpy
-from plotly import express
-from pandas import DataFrame
-from scipy.signal import decimate, detrend
-
-
 def rms(data, axis=-1):
     """
     returns the Root Mean Square (RMS) value of data along the given axis
@@ -16,7 +10,7 @@ def rms(data, axis=-1):
 
 
 def sig_plotly(time_array, signals_array, channels_array, samp_freq, file_name,
-               do_decimate=True, do_normalize=True, do_detrend="linear"):
+              plot_title=None, do_decimate=True, do_normalize=True, do_detrend="linear"):
     """
     Creates (exports) the signals as an HTML plotly plot
 
@@ -26,6 +20,7 @@ def sig_plotly(time_array, signals_array, channels_array, samp_freq, file_name,
         channels_array: numpy array (or list) of channel names
         samp_freq: sampling frequency (Hz)
         file_name: name (and directory) for the exported html file
+        plot_title: Plot title (default is None)
         do_decimate: down-sampling (decimating) the signal to 200Hz sampling rate
             (default and recommended value is True)
         do_normalize: dividing the signal by the root mean square value for normalization
@@ -60,9 +55,41 @@ def sig_plotly(time_array, signals_array, channels_array, samp_freq, file_name,
                        line_shape="spline", render_mode="svg",
                        labels=dict(index="Time (s)",
                                    value="(a.u.)",
-                                   variable="Channel"))
+                                   variable="Channel"), title=plot_title)
     fig.update_layout(yaxis=dict(tickmode='array',
                                  tickvals=offset_value * numpy.arange(len(channels_array)),
                                  ticktext=channels_array))
 
     fig.write_html(str(file_name) + ".html")
+
+
+def raw_plotly(mne_raw, file_name, t_slice=(), plot_title=None,
+               do_decimate=True, do_normalize=True, do_detrend="linear"):
+    """
+    Creates (exports) the (sliced) MNE raw signal as an HTML plotly plot
+
+    Arguments:
+        mne_raw: MNE raw object (output of mne.io.read_raw_...)
+        file_name: name (and directory) for the exported html file
+        t_slice: tuple of start and end slice (seconds)
+        plot_title: Plot title (default is None)
+        do_decimate: down-sampling (decimating) the signal to 200Hz sampling rate
+            (default and recommended value is True)
+        do_normalize: dividing the signal by the root mean square value for normalization
+            (default and recommended value is True)
+        do_detrend: The type of detrending.
+            If do_detrend == 'linear' (default), the result of a linear least-squares fit to data is subtracted from data.
+            If do_detrend == 'constant', only the mean of data is subtracted.
+            else, no detrending
+    
+    returns nothing
+    """
+    samp_freq = int(mne_raw.info["sfreq"])
+    channels_array = numpy.array(mne_raw.info["ch_names"])
+    if t_slice:
+        signals_array, time_array = mne_raw[:, t_slice[0]*samp_freq:t_slice[1]*samp_freq]
+    else:
+        signals_array, time_array = mne_raw[:, :]
+
+    sig_plotly(time_array, signals_array, channels_array, samp_freq, file_name,
+               do_decimate, do_normalize, do_detrend)
