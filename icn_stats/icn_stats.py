e35b4b7747f57f35bf4cabf0ac10f4f092fac330
Richard Koehler <richard.koehler@outlook.de>
2021-08-06 15:26:27 +0200

    Implement cluster-wise p-val correction for numba

--- icn_stats/icn_stats.py
+++ icn_stats/icn_stats.py
@@ -9,6 +9,7 @@ import scipy.stats as stats
 import seaborn as sns
 
 
+from skimage import measure
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import KFold
 import statsmodels.api as sm
@@ -287,48 +288,61 @@ def permutation_numba_twosample(x, y, n_perm, two_tailed=True):
 
 
 def cluster_wise_p_val_correction(p_arr, p_sig=0.05, num_permutations=10000):
-    """
+    """Obtain cluster-wise corrected p values.
 
     Based on: https://github.com/neuromodulation/wjn_toolbox/blob/4745557040ad26f3b8498ca5d0c5d5dece2d3ba1/mypcluster.m
     https://garstats.wordpress.com/2018/09/06/cluster/
 
-    Obtain cluster-wise corrected p values.
-
+    Arguments
+    ---------
     p_arr (np.array) : ndim, can be time series or image
     p_sig (float) : significance level
     num_permutations (int) : no. of random permutations of cluster comparisons
 
-    returns:
+    Returns
+    -------
     p (float) : significance level of highest cluster
     p_min_index : indices of significant samples
     """
-    labels, num_clusters = measure.label(p_arr < p_sig, return_num=True)
+    labels, num_clusters = measure.label(p_arr <= p_sig, return_num=True)
 
     # loop through clusters of p_val series or image
     index_cluster = {}
     p_cluster_sum = np.zeros(num_clusters)
-    for cluster_i in range(num_clusters):
-        index_cluster[cluster_i] = np.where(labels == cluster_i+1)[0]  # first cluster is assigned to be 1 from measure.label
-        p_cluster_sum[cluster_i] = np.sum(np.array(1-p_arr)[index_cluster[cluster_i]])
-    p_min = np.max(p_cluster_sum)  # p_min corresponds to the most unlikely cluster
+    for cluster_i in np.arange(num_clusters):
+        # first cluster is assigned to be 1 from measure.label
+        index_cluster[cluster_i] = np.where(labels == cluster_i + 1)[0]
+        p_cluster_sum[cluster_i] = np.sum(
+            np.array(1 - p_arr)[index_cluster[cluster_i]])
+    # p_min corresponds to the most unlikely cluster
+    p_min = np.max(p_cluster_sum)
+
     p_min_index = index_cluster[np.argmax(p_cluster_sum)]
 
     # loop through random permutation cycles
     r_per_arr = np.zeros(num_permutations)
     for r in range(num_permutations):
-        r_per = np.random.randint(low=0, high=p_arr.shape[0], size=p_arr.shape[0])
+        r_per = np.random.randint(low=0, high=p_arr.shape[0],
+                                  size=p_arr.shape[0])
 
-        labels, num_clusters = measure.label(p_arr[r_per] < p_sig, return_num=True)
+        labels, num_clusters = measure.label(p_arr[r_per] <= p_sig,
+                                             return_num=True)
 
         index_cluster = {}
-        p_cluster_sum = np.zeros(num_clusters)
-        for cluster_i in range(num_clusters):
-            index_cluster[cluster_i] = np.where(labels == cluster_i+1)[0]  # first cluster is assigned to be 1 from measure.label
-            p_cluster_sum[cluster_i] = np.sum(
-                np.array(1-p_arr[r_per])[index_cluster[cluster_i]])
-        r_per_arr[r] = np.max(p_cluster_sum)  # corresponds to the most unlikely cluster
+        if num_clusters == 0:
+            r_per_arr[r] = 0
+        else:
+            p_cluster_sum = np.zeros(num_clusters)
+            for cluster_i in np.arange(num_clusters):
+                index_cluster[cluster_i] = np.where(
+                    labels == cluster_i + 1)[
+                    0]  # first cluster is assigned to be 1 from measure.label
+                p_cluster_sum[cluster_i] = np.sum(
+                    np.array(1 - p_arr[r_per])[index_cluster[cluster_i]])
+            # corresponds to the most unlikely cluster
+            r_per_arr[r] = np.max(p_cluster_sum)
 
-        sorted_r = np.sort(r_per_arr)
+    sorted_r = np.sort(r_per_arr)
 
     def find_arg_nearest(array, value):
         array = np.asarray(array)
@@ -338,3 +352,118 @@ def cluster_wise_p_val_correction(p_arr, p_sig=0.05, num_permutations=10000):
     p = 1 - find_arg_nearest(sorted_r, p_min) / num_permutations
 
     return p, p_min_index
+
+
+@njit
+def cluster_wise_p_val_correction_numba(p_arr, p_sig, n_perm):
+    """Calculate significant clusters and their corresponding p-values.
+
+    Based on:
+    https://github.com/neuromodulation/wjn_toolbox/blob/4745557040ad26f3b8498ca5d0c5d5dece2d3ba1/mypcluster.m
+    https://garstats.wordpress.com/2018/09/06/cluster/
+
+    Arguments
+    ---------
+    p_arr :  array-like
+        Array of p-values. WARNING: MUST be one-dimensional
+    p_sig : float
+        Significance level
+    n_perm : int
+        No. of random permutations for building cluster null-distribution
+
+    Returns
+    -------
+    p : list of floats
+        List of p-values for each cluster
+    p_min_index : list of numpy array
+        List of indices of each significant cluster
+    """
+
+    def cluster(iterable):
+        """Cluster 1-D array of boolean values.
+
+        Parameters
+        ----------
+        iterable : array-like of bool
+            Array to be clustered.
+
+        Returns
+        -------
+        cluster_labels : np.array
+            Array of shape (len(iterable), 1), where each value indicates the
+            number of the cluster. Values are 0 if the item does not belong to
+            a cluster
+        cluster_count : int
+            Number of detected cluster. Corresponds to the highest value in
+            cluster_labels
+        """
+        cluster_labels = np.zeros((len(iterable), 1))
+        cluster_count = 0
+        cluster_len = 0
+        for idx, item in enumerate(iterable):
+            if item:
+                cluster_labels[idx] = cluster_count + 1
+                cluster_len += 1
+            elif cluster_len == 0:
+                pass
+            else:
+                cluster_len = 0
+                cluster_count += 1
+        if cluster_len >= 1:
+            cluster_count += 1
+        return cluster_labels, cluster_count
+
+    def calculate_null_distribution(p_arr_, p_sig_, n_perm_):
+        """Calculate null distribution of clusters.
+
+        Parameters
+        ----------
+        p_arr_ :  numpy array
+            Array of p-values
+        p_sig_ : float
+            Significance level (p-value)
+        n_perm_ : int
+            No. of random permutations
+
+        Returns
+        -------
+        r_per_arr : numpy array
+            Null distribution of shape (n_perm_)
+        """
+        # loop through random permutation cycles
+        r_per_arr = np.zeros(n_perm_)
+        for r in range(n_perm_):
+            r_per = np.random.randint(
+                low=0, high=p_arr_.shape[0], size=p_arr_.shape[0])
+            labels_, n_clusters = cluster(p_arr_[r_per] <= p_sig_)
+
+            cluster_ind = {}
+            if n_clusters == 0:
+                r_per_arr[r] = 0
+            else:
+                p_sum = np.zeros(n_clusters)
+                for ind in range(n_clusters):
+                    cluster_ind[ind] = \
+                        np.where(labels_ == ind + 1)[0]
+                    p_sum[ind] = np.sum(
+                        np.asarray(1 - p_arr_[r_per])[cluster_ind[ind]])
+                r_per_arr[r] = np.max(p_sum)
+        return r_per_arr
+
+    labels, num_clusters = cluster(p_arr <= p_sig)
+
+    null_distr = calculate_null_distribution(p_arr, p_sig, n_perm)
+    # Loop through clusters of p_val series or image
+    clusters = []
+    p_vals = [np.float64(x) for x in range(0)]
+    # Cluster labels start at 1
+    for cluster_i in range(num_clusters):
+        index_cluster = np.where(labels == cluster_i + 1)[0]
+        p_cluster_sum = np.sum(np.asarray(1 - p_arr)[index_cluster])
+        p_val = 1 - np.sum(p_cluster_sum >= null_distr) / n_perm
+        print(np.sum(p_cluster_sum >= null_distr))
+        if p_val <= p_sig:
+            clusters.append(index_cluster)
+            p_vals.append(p_val)
+
+    return p_vals, clusters
