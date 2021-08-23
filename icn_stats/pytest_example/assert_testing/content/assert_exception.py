eb491ad759fe4733a0b5ad42dd2034d14ec5fa46
Jonathan Vanhoecke <72917013+JonathanVHoecke@users.noreply.github.com>
2021-08-23 16:39:18 +0200

    mini-Demo on pytest

---
+++ icn_stats/pytest_example/assert_testing/content/assert_exception.py
@@ -0,0 +1,18 @@
+
+def divide(denominator):
+    try:
+        outcome = 10 / denominator
+
+    except ZeroDivisionError as inst:
+        print("denominator cannot be 0")
+        outcome = type(inst).__name__
+    except TypeError as inst:
+        print("denominator must be a number")
+        outcome = type(inst).__name__
+
+    return outcome
+
+
+
+
+
