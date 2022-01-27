4eb078438c0a382dd91aa7c5009b2e31250d3ce2
jgvhabets <jgvhabets@gmail.com>
2022-01-27 18:17:49 +0100

    Added new task from Dyskinesia-Protocol to Task-Descriptions and Task-Instructions containers.

--- icn_bids/BIDS_Berlin_ECOG_LFP/0.11/convert_531AI63_MedOff1_SelfpRotaR_StimOff_20220124T093627.m
+++ icn_bids/BIDS_Berlin_ECOG_LFP/0.11/convert_531AI63_MedOff1_SelfpRotaR_StimOff_20220124T093627.m
@@ -285,7 +285,7 @@ cfg.dataset_description.Name                = 'BIDS_Berlin_LFP_ECOG_PD';
 cfg.dataset_description.BIDSVersion         = '1.5.0';
 cfg.dataset_description.License             = 'n/a';
 cfg.dataset_description.Funding             = [['Deutsche Forschungsgemeinschaft (DFG, German Research Foundation) - Project-ID 424778381 - TRR 295']];
-cfg.dataset_description.Authors             = {'Johannes Busch', 'Meera Chikermane', 'Katharina Faust', 'Lucia Feldmann', 'Richard Koehler', 'Andrea Kuehn', 'Roxanne Lofredi', 'Timon Merk', 'Wolf-Julian Neumann', 'Gerd-Helge Schneider', 'Ulrike Uhlig','Jonathan Vanhoecke'};
+cfg.dataset_description.Authors             = {'Johannes Busch', 'Meera Chikermane', 'Katharina Faust', 'Lucia Feldmann', 'Richard Koehler', 'Andrea Kuehn', 'Roxanne Lofredi', 'Timon Merk', 'Wolf-Julian Neumann', 'Gerd-Helge Schneider', 'Ulrike Uhlig','Jeroen Habets','Jonathan Vanhoecke'};
 cfg.dataset_description.Acknowledgements    = 'Special thanks to Ulrike Uhlig for their help in recording the data.';
 
 % Provide the long description of the task and participant instructions
