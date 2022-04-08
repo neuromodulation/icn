1685960a43d515a20bb93818c5c01bbd912e1ad3
Jonathan Vanhoecke <72917013+JonathanVHoecke@users.noreply.github.com>
2022-04-08 18:20:16 +0200

    extra tutorial description

--- tutorials/atlas_to_PET/apply_atlas_to_PET_scan.m
+++ tutorials/atlas_to_PET/apply_atlas_to_PET_scan.m
@@ -1,22 +1,29 @@
-%% Converting abagen expression map of 1 gene of interest to nifti
+%% Apply a new atlas to an image
+% Here the atlas HCPex_SUIT_AGBT was applied to the PET scan,
+% which means that the scan was masked.
+% Each parcel of the new image is now the average voxel intensity
+% of the corresponding parcel on the PET scan.
 % 08.04.2022 - JVH
 
 %% SPM is needed for Lead DBS, which we use to read and write niftis
 addpath('C:\Users\Jonathan\Documents\MATLAB\add_on_Matlab\spm12')
 addpath(genpath('C:\Users\Jonathan\Documents\MATLAB\add_on_Matlab\lead'))
 
-
+%% load the atlas, the PET scan of interest, and define the output
 nii_atlas = ea_load_nii('C:\Users\Jonathan\Documents\DATA\ATLAS_creation\compound_atlas_HCPex_SUIT_ABGT.nii');
 nii_PET = ea_load_nii('C:\Users\Jonathan\Documents\CODE\hansen_receptors\data\PET_nifti_images\DAT_fepe2i_hc6_sasaki.nii.gz');
 nii_out = nii_atlas;
 
+%% read in the accopanying area label table and define the output
 areas = readtable('C:\Users\Jonathan\Documents\DATA\ATLAS_creation\compound_atlas_HCPex_SUIT_ABGT.txt');
 areas_out = table({'id'},{'intensity'},{'label'});
 
 
-nii_out.img = nii_PET.img(nii_atlas.img>0); %images MUST have the same dimension
+%% set the output to zero (or let it take the values of the PET scan, when you do not want to average)
+%nii_out.img = nii_PET.img(nii_atlas.img>0); %images MUST have the same dimension
 nii_out.img = zeros(nii_out.dim); % set the out image to zeros
 
+%% each parcel of the atlas, apply it to the PET scan, then average and sum it to the output
 for p = 1:length(unique(nii_atlas.img))-1
     parcel = zeros(nii_out.dim);
     avg_intensity =  mean(nii_PET.img(nii_atlas.img==p));
@@ -27,6 +34,7 @@ for p = 1:length(unique(nii_atlas.img))-1
     areas_out = [areas_out;{p,avg_intensity,areas.Var2{p}}];
 end
 
+%% pint out the output
 nii_out.fname = 'compound_atlas_DAT_fepe2i_hc6_sasaki.nii';
 nii_out.pinfo = [0;0;352];
 ea_write_nii(nii_out);
--- tutorials/gene_expression_map/retrieve_nifti_for_gene_of_interest.m
+++ tutorials/gene_expression_map/retrieve_nifti_for_gene_of_interest.m
@@ -1,21 +1,30 @@
 %% Converting abagen expression map of 1 gene of interest to nifti
 % 08.04.2022 - JVH
 
+%% load the atlas you want to apply on the gene sampling map created by abagen toolbox
 nii_atlas = ea_load_nii('C:\Users\Jonathan\Documents\DATA\ATLAS_creation\compound_atlas_HCPex_SUIT_ABGT.nii');
 gene_expression_map_fname = 'C:\Users\Jonathan\Documents\PYCHARM\Python\abagen_HCPex_SUIT_ABGT_expression_map.csv';
 
+%% the genetic map is so large, that csvread does not work. Use datastore
 ds = datastore(gene_expression_map_fname, 'MissingValue',0); 
 % Choose data of interest and data types
 ds.SelectedVariableNames = {'label','CSF1R'};
 gene_data = readall(ds);
 
+%% here, we want to extract CSF1R and apply its values to the atlas
+% we use the atlas, change its name and its voxel intensities to those of
+% CSF1R
 nii_atlas.fname = 'expression_CSF1R.nii';
 nii_atlas.img = changem(nii_atlas.img,gene_data.('CSF1R'),gene_data.('label'));
 
+% change the planar info so the raw values remain. The 3 element depends on
+% atlas used
 nii_atlas.pinfo = [0;0;352];
+
+%% write out the expression map
 ea_write_nii(nii_atlas);
 
+%% check whether the values correspond
 nii_expression_CSF1R = ea_load_nii('expression_CSF1R.nii');
-
 unique(nii_expression_CSF1R.img)
 %spm_imcalc(nii_atlas.fname,'gene_map_CSF1R.nii','(i1>0).*(i2>0)')
