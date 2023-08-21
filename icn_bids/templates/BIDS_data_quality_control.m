75e51fadcce04e98836e1233d61ca4a1f4e338fa
Jonathan Vanhoecke <72917013+JonathanVHoecke@users.noreply.github.com>
2023-08-21 12:26:06 +0200

    Update BIDS_data_quality_control.m

--- icn_bids/templates/BIDS_data_quality_control.m
+++ icn_bids/templates/BIDS_data_quality_control.m
@@ -15,7 +15,7 @@ fg = figure(1);
 % this is where the meta json files are located
 cd('C:\Users\Jonathan\Documents\DATA\PROJECT_Berlin_dev\')
 % This is the output root folder for our BIDS-dataset
-rawdata_root = 'C:\Users\Jonathan\Documents\DATA\PROJECT_BERLIN_dev\rawdata_update4\';
+rawdata_root = 'C:\Users\Jonathan\Documents\DATA\PROJECT_BERLIN_dev\rawdata_update5\';
  % This is the input root folder for our BIDS-dataset
 % sourcedata_root = 'C:\Users\Jonathan\Documents\DATA\PROJECT_BERLIN_dev\rawdata10c\';
 sourcedata_root = 'C:\Users\Jonathan\Documents\CODE\icn\icn_bids\';
